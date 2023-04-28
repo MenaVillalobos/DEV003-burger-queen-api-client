5f481d98647f1725d71c916ba21c7fbe9611df6d
MenaVillalobos <jimee69styles@gmail.com>
2023-04-27 20:19:27 -0600

    chef view layout

--- src/ChefView/index.js
+++ src/ChefView/index.js
@@ -1,9 +1,43 @@
+import './index.css'
+import redHeader from '../imgsBQ/UserHist2/redHeaderMenuView.png'
+import femChef from '../imgsBQ/UserHist3/femChef1stOpt.png'
+import chef from '../imgsBQ/UserHist3/maleChef1stOpt.png'
 
 function ChefView() {
     return(
-        <div>
-            <h1>AQUÍ VA LA VISTA DEL JEFE DE COCINA</h1>
-            <h2>Estoy tratando de hacer una ruta nueva</h2>
+        <div className="chefViewGralContainer">
+            <div className="redHeader">
+                <img className='headerImg' src={redHeader} alt='logo del header'/>
+                <div className='welcomeKitchenLetter'>
+                    ¡Bienvenidx a cocina!
+                </div>
+                <div className='chefsContainer'>
+                    <img className='headerFemChef' src={femChef} alt='logo header chef fem'/>
+                    <img className='headerChef' src={chef} alt='logo header chef'/>
+                </div>
+            </div>
+            <div className='ticketsContainer'>
+                <div className='ticketsViewContainer'>
+                    <div className='ticketsViewLetter'>
+                        <p className='ticketsViewTxt1'>
+                            ÓRDENES
+                        </p>
+                        <p className='ticketsViewTxt2'>
+                            RECIBIDAS
+                        </p>
+                    </div>
+                </div>
+                <div className='ticketsToDeliveryContainer'>
+                    <div className='ticketsDeliveryLetter'>
+                        <p className='deliveryTxt1'>
+                            ÓRDENES
+                        </p>
+                        <p className='deliveryTxt2'>
+                            LISTAS
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
